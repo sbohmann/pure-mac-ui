7db3cc28f6342b26640cb454e604f4f8964e08c1
sbohmann <github@yeoman.at>
2023-11-11 13:00:36 +0100

    Initial Commit

---
+++ pure-mac-ui/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  pure-mac-ui
+//
+//  Created by seb on 2023-11-11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
